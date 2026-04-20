973a836e62eef7db049dbc951971aa0c030eaa65
Jeevan M Swamy <jeevanms003@gmail.com>
2026-04-20 14:38:38 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int x: nums)
+        {
+            mp[x] ++;
+            if(mp[x] >1)
+            {
+                return x;
+            }
+        }
+        
+        
+    }
+};
